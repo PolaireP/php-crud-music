b9e9f0144b3788ac1234f62bb9644db2ff07c284
Romain Poirrier <romain.poirrier@etudiant.univ-reims.fr>
2023-05-05 12:11:02 +0200

    8.2.8 (aucune redirection, fi nde séance)

--- public/artist.php
+++ public/artist.php
@@ -4,36 +4,45 @@ declare(strict_types=1);
 use Database\MyPdo;
 use Html\WebPage;
 
-$artistId = 17;
+if(isset($_GET['artistId']) && ctype_digit($_GET['artistId'])) {
+    $artistId = $_GET['artistId'];
 
-$requete = MyPDO::getInstance()->prepare(<<<SQL
-    SELECT name
-    FROM artist
-    WHERE id = {$artistId};
-SQL);
+    $requete = MyPDO::getInstance()->prepare(<<<SQL
+        SELECT name
+        FROM artist
+        WHERE id = {$artistId};
+    SQL);
 
-$requete->execute();
+    $requete->execute();
 
-$webpage = new WebPage();
+    $webpage = new WebPage();
+    if ($requete->rowCount() == 0) {
+        http_response_code(404);
+        exit();
+    }
+    else {
+        $nom = $webpage->escapeString($requete->fetch()['name']);
+        $webpage->setTitle("Album de " . $nom);
 
-$nom = $webpage->escapeString($requete->fetch()['name']);
-$webpage->setTitle("Album de ". $nom);
+        $reqAlbums = MyPDO::getInstance()->prepare(
+            <<<SQL
+            
+            SELECT year, name
+            FROM album
+            WHERE artistID = {$artistId}
+            ORDER BY year DESC, name;
+            
+        SQL
+        );
 
-$reqAlbums = MyPDO::getInstance()->prepare(
-    <<<SQL
-    
-    SELECT year, name
-    FROM album
-    WHERE artistID = {$artistId}
-    ORDER BY year DESC, name;
-    
-SQL
-);
+        $reqAlbums->execute();
 
-$reqAlbums->execute();
-
-while (($ligne = $reqAlbums->fetch()) !== false) {
-    $webpage ->appendContent("<p>". $ligne['year']. ' '. $webpage->escapeString($ligne['name']));
+        while (($ligne = $reqAlbums->fetch()) !== false) {
+            $webpage->appendContent("<p>" . $ligne['year'] . ' ' . $webpage->escapeString($ligne['name']));
+        }
+    }
+    echo $webpage->toHTML();
+} else {
+    header('Location: index.php', response_code: 302);
+    exit();
 }
-
-echo $webpage->toHTML();
