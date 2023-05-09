d7a2d062068e88825fe1ae7abfe234c97e43667a
Romain Poirrier <romain.poirrier@etudiant.univ-reims.fr>
2023-05-09 14:39:51 +0200

    Fin du 2.4.4, ajout de la classe ArtistCollection

---
+++ src/Entity/Collection/ArtistCollection.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collection;
+
+use Database\MyPdo;
+use Entity\Artist;
+use PDO;
+
+class ArtistCollection
+{
+    /**
+     * Méthode permettant de récupérer tous les artistes de la base de donnée
+     * avec leur id et leur nom.
+     *
+     * @return Artist[] Renvoi d'un tableau d'entité Artist
+     */
+    public function findAll(): array
+    {
+        $request = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT id, nom
+            FROM artist
+            ORDER BY nom
+        SQL
+        );
+
+        $request->execute();
+
+        return $request->fetchAll(PDO::FETCH_CLASS, Artist::class);
+    }
+}
